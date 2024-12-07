5309b64c632829d3a97cb4034d621869f285102a
nabellashukri <ainnuralisnabella@graduate.utm.my>
2024-12-07 17:26:10 +0800

    nabella - update timetable container and link to timetable page

--- FLOWSYNC_Project/resources/views/dashboard.blade.php
+++ FLOWSYNC_Project/resources/views/dashboard.blade.php
@@ -10,23 +10,23 @@
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4 justify-center">
-        <!-- FIRST BOX -->
+        <!-- FIRST BOX (TIMETABLE) -->
         <div class="p-4 lg:w-1/3">
-            <div class="h-full bg-gradient-to-r from-red-100 to-red-300 hover:from-red-200 hover:to-red-400 transition-all duration-300 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg hover:shadow-2xl">
+            <a href="{{ route('timetable') }}" class="block h-full bg-gradient-to-r from-red-100 to-red-300 hover:from-red-200 hover:to-red-400 transition-all duration-300 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg hover:shadow-2xl">
                 <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">TIMETABLE</h1>
                 <p class="leading-relaxed mb-3">Semester 1 Session 2024/2025</p>
-                <a class="text-black-700 inline-flex items-center font-bold hover:underline">Learn More
+                <span class="text-black-700 inline-flex items-center font-bold hover:underline">Learn More
                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M5 12h14"></path>
                         <path d="M12 5l7 7-7 7"></path>
                     </svg>
-                </a>
-            </div>
+                </span>
+            </a>
         </div>
 
-        <!-- SECOND BOX -->
+        <!-- SECOND BOX (CALENDAR) -->
         <div class="p-4 lg:w-1/3">
-        <div class="h-full bg-gradient-to-r from-red-100 to-red-300 hover:from-red-200 hover:to-red-400 transition-all duration-300 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg hover:shadow-2xl">
+            <div class="h-full bg-gradient-to-r from-red-100 to-red-300 hover:from-red-200 hover:to-red-400 transition-all duration-300 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg hover:shadow-2xl">
                 <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">CALENDAR</h1>
                 <p class="leading-relaxed mb-3">Google Calendar Integration</p>
                 <a class="text-black-700 inline-flex items-center font-bold hover:underline">Learn More
