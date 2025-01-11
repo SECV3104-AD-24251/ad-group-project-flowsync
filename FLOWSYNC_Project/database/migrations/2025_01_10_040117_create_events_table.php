f941ba51a533055ce99dd900b7dc9cafd8170d22
nurirdinasyafiqah <99268943+nurirdinasyafiqah@users.noreply.github.com>
2025-01-11 23:58:54 +0800

    update migrate database Student Calendar

--- FLOWSYNC_Project/database/migrations/2025_01_10_040117_create_events_table.php
+++ FLOWSYNC_Project/database/migrations/2025_01_10_040117_create_events_table.php
@@ -7,18 +7,16 @@ use Illuminate\Support\Facades\Schema;
 class CreateEventsTable extends Migration
 {
     public function up()
-{
-    Schema::create('events', function (Blueprint $table) {
-        $table->id();
-        $table->string('title');
-        $table->text('description')->nullable(); // Add description column
-        $table->timestamp('start');
-        $table->timestamp('end');
-        $table->timestamps();
-    });
-}
-
-
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable(); // Description column
+            $table->timestamp('start')->nullable(); // Allow NULL values for 'start'
+            $table->timestamp('end')->nullable(); // Allow NULL values for 'end'
+            $table->timestamps();
+        });
+    }
 
     public function down()
     {
